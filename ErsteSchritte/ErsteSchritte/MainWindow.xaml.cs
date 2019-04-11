7bad629183082f33a1c79c76f83fc5febcd84ec2
Jörn Loviscach <jl@j3L7h.de>
2019-04-11 16:59:08 +0200

    Stand nach Veranstaltung

--- ErsteSchritte/ErsteSchritte/MainWindow.xaml.cs
+++ ErsteSchritte/ErsteSchritte/MainWindow.xaml.cs
@@ -15,14 +15,53 @@ using System.Windows.Shapes;
 
 namespace ErsteSchritte
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button geklickt");
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Title = "Maus war über Button";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MwStAusrechnen();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            MwStAusrechnen();
+        }
+
+        private void MwStAusrechnen()
+        {
+            // Bei Aufbau des Fensters werden schon Ereignisse ausgelöst, 
+            // noch bevor alle Komponenten bereit stehen.
+            // In diesem Fall können wir noch nichts tun. Also:
+            if (radioButton7 == null || textBox.Text == null || textBlock == null)
+            {
+                return; 
+            }
+
+            double x = double.Parse(textBox.Text);
+            if (radioButton7.IsChecked == true)
+            {
+                x *= 1.07;
+            }
+            else
+            {
+                x *= 1.19;
+            }
+            textBlock.Text = x.ToString();
         }
     }
 }
