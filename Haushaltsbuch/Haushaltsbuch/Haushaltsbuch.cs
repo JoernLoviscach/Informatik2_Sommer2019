6671c22ac26a0f3fa15a6432444eae3c57dc00ea
Jörn Loviscach <jl@j3L7h.de>
2019-05-09 16:39:08 +0200

    Stand nach Veranstaltung

---
+++ Haushaltsbuch/Haushaltsbuch/Haushaltsbuch.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haushaltsbuch
+{
+    abstract class Buchungsposten
+    {
+        decimal betrag;
+        public decimal Betrag { get { return betrag; } }
+        DateTime zeitpunkt;
+        public DateTime Zeitpunkt { get { return zeitpunkt; } }
+        string beschreibung;
+        public string Beschreibung { get { return beschreibung; } }
+        public Buchungsposten(decimal betrag, DateTime zeitpunkt, string beschreibung)
+        {
+            this.betrag = betrag;
+            this.zeitpunkt = zeitpunkt;
+            this.beschreibung = beschreibung;
+        }
+        public abstract decimal BerechneGesamtbetragBis(DateTime datum);
+    }
+
+    class EinmaligeBuchung : Buchungsposten
+    {
+        public EinmaligeBuchung(decimal betrag, DateTime zeitpunkt, string beschreibung)
+            : base(betrag, zeitpunkt, beschreibung)
+        {
+        }
+        public override decimal BerechneGesamtbetragBis(DateTime datum)
+        {
+            //if(datum < Zeitpunkt)
+            //{
+            //    return 0;
+            //}
+            //return Betrag;
+            return (datum < Zeitpunkt) ? 0 : Betrag;
+        }
+    }
+
+    class MonatlicheBuchung : Buchungsposten
+    {
+        DateTime endzeitpunkt;
+        public MonatlicheBuchung(decimal betrag, DateTime zeitpunkt, DateTime endzeitpunkt, string beschreibung)
+            : base(betrag, zeitpunkt, beschreibung)
+        {
+            this.endzeitpunkt = endzeitpunkt;
+        }
+        public override decimal BerechneGesamtbetragBis(DateTime datum)
+        {
+            decimal summe = 0;
+            // TODO: Monate durchgehen, nicht immer 30 Tage
+            for (DateTime d = Zeitpunkt; d <= endzeitpunkt; d += TimeSpan.FromDays(30))
+            {
+                if(d <= datum)
+                {
+                    summe += Betrag;
+                }
+            }
+            return summe;
+        }
+    }
+
+    class Kontobuch
+    {
+        List<Buchungsposten> buchungen = new List<Buchungsposten>();
+        public Kontobuch()
+        {
+        }
+        public void FügeBuchungHinzu(Buchungsposten buchung)
+        {
+            buchungen.Add(buchung);
+        }
+        public decimal BerechneStandAm(DateTime datum)
+        {
+            decimal kontostand = 0;
+            for (int i = 0; i < buchungen.Count; i++)
+            {
+                kontostand += buchungen[i].BerechneGesamtbetragBis(datum);
+            }
+            return kontostand;
+        }
+    }
+}
---
+++ Haushaltsbuch/Haushaltsbuch/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Haushaltsbuch
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EinmaligeBuchung b1 = new EinmaligeBuchung(42, new DateTime(2019, 5, 2), "A");
+            MonatlicheBuchung b2 = new MonatlicheBuchung(100, new DateTime(2010, 1, 1), new DateTime(2020, 1, 4), "B");
+            Kontobuch k = new Kontobuch();
+            k.FügeBuchungHinzu(b1);
+            k.FügeBuchungHinzu(b2);
+            decimal d1 = k.BerechneStandAm(new DateTime(2019, 4, 4));
+            decimal d2 = k.BerechneStandAm(new DateTime(2019, 5, 5));
+            decimal d3 = k.BerechneStandAm(new DateTime(2019, 6, 6));
+        }
+    }
+}
